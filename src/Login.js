194e56f956cd2568ec89699df6050a0c2f30b36c
Justin <justinkmpeter@gmail.com>
2022-08-17 13:02:00 -0500

    Making the login page look 😮‍💨 beautiful

--- src/Login.js
+++ src/Login.js
@@ -4,9 +4,12 @@ import './Login.css'
 function Login() {
     return (
         <div className="login">
-            {/* Spotify Logo */}
-            {/* Login w/ spotify  */}
-            <h1> I am the login page</h1>
+
+            <img
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+                alt=""
+            />
+            <a> LOGIN WITH SPOTIFY </a>
         </div>
     );
 }
