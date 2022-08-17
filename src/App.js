6ff79e96cdcbb1732e356bdf0ab5d30e68fd8002
Justin <justinkmpeter@gmail.com>
2022-08-17 12:42:36 -0500

    Setting up the login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Login from './Login'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1> Spotify.... we're coming!! </h1>
+
+        {/* Spotify Logo*/}
+        {/* Login w/ Spotify Button */}
+        <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            {/* Spotify Logo */}
+            {/* Login w/ spotify  */}
+            <h1> I am the login page</h1>
+        </div>
+    );
+}
+
+export default Login;
