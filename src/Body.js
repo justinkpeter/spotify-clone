a78f4277e1837fd285550e9cd550a41848e6be67
Justin <justinkmpeter@gmail.com>
2022-08-22 12:46:56 -0500

    Added some styling to the header & body

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Body.css'
+import Header from './Header'
 
-function Body(props) {
+function Body({spotify}) {
     return (
         <div className="body">
-            <h1> I am the body</h1>
+            <Header spotify={spotify} />
+
+            <div className="body__info">
+                <img src="https://newjams-images.scdn.co/image/ab676477000033ad/dt/v3/discover-weekly/8PtZiVP0nL008X12wn646TW6A_h6WDiUdWvgHU6uhYcQrEXDWLFqWPqrlW_PyW1TL_2WiNDEjeb9A85-9xDZsDUF73W6tyd7Pl1vLUzqy04=/MjM6ODU6MDBUNzItNjAtMg==" alt=""/>
+                <div className="body__infoText">
+                    <strong> PLAYLIST </strong>
+                    <h2> Discover Weekly </h2>
+                    <p> description .... </p>
+
+                </div>
+            </div>
         </div>
     );
 }
